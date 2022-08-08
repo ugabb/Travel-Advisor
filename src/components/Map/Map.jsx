49db786e7565bf37ac6ab6662c32ae3110036109
Gabriel Barros <gsb.alternativo@gmail.com>
2022-08-08 17:40:18 -0300

    first commit before start to work with APIs

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react' 
+import { Paper, Typography, useMediaQuery} from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import Rating from '@material-ui/lab'
+
+import useStyles from './style'
+
+const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+  const coordinates = {lat : 0, lng:0};
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{key:'AIzaSyBqNesU8pBPsq4n0QC9MTNtjhx5p7GVa18'}}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
+  )
+}
+
+export default Map
